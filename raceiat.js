1bd02fddc5a62c4f4f085b88eaebe931d6a0b4f8
KatBrad <160325347+KatBrad@users.noreply.github.com>
2024-05-06 18:47:07 +0300

    Update raceiat.js

--- raceiat.js
+++ raceiat.js
@@ -3,78 +3,82 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
     let global = API.getGlobal();
 
     return iatExtension({
-        category1 : {
-            name : global.blackLabels, //Will appear in the data.
-            title : {
-                media : {word : global.blackLabels}, //Name of the category presented in the task.
-                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
-                height : 4 //Used to position the "Or" in the combined block.
-            }, 
-            stimulusMedia : [ //Stimuli content as PIP's media objects
-                {image: 'bm1_nc.jpg'},
-                {image: 'bm2_nc.jpg'},
-                {image: 'bm3_nc.jpg'},
-                {image: 'bf1_nc.jpg'},
-                {image: 'bf2_nc.jpg'},                 
-                {image: 'bf3_nc.jpg'}     
-            ],
-            //Stimulus css (style)
-            stimulusCss : {color:'#31940F','font-size':'2.3em'}
-        },    
-        category2 : {
-            name : global.whiteLabels, //Will appear in the data.
-            title : {
-                media : {word : global.whiteLabels}, //Name of the category presented in the task.
-                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
-                height : 4 //Used to position the "Or" in the combined block.
-            }, 
-            stimulusMedia : [ //Stimuli content as PIP's media objects
-                {image: 'wm1_nc.jpg'},
-                {image: 'wm2_nc.jpg'},
-                {image: 'wm3_nc.jpg'},
-                {image: 'wf1_nc.jpg'},
-                {image: 'wf2_nc.jpg'},
-                {image: 'wf3_nc.jpg'}
-            ],
-            //Stimulus css (style)
-            stimulusCss : {color:'#31940F','font-size':'2.3em'}
-        },
-        attribute1 : {
-            name : 'Bad words',
-            title : {
-                media : {word : 'Bad words'},
-                css : {color:'#0000FF','font-size':'1.8em'},
-                height : 4 //Used to position the "Or" in the combined block.
-            },
-            stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: global.negWords[0]},
-                {word: global.negWords[1]},
-                {word: global.negWords[2]},
-                {word: global.negWords[3]},
-                {word: global.negWords[4]},
-                {word: global.negWords[5]},
-                {word: global.negWords[6]},
-                {word: global.negWords[7]}
-            ],
-            //Stimulus css
-            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-        },
-        attribute2 : {
-            name : 'Good words',
-            title : {
-                media : {word : 'Good words'},
-                css : {color:'#0000FF','font-size':'1.8em'},
-                height : 4 //Used to position the "Or" in the combined block.
-            },
-            stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: global.posWords[0]},
-                {word: global.posWords[1]},
-                {word: global.posWords[2]},
-                {word: global.posWords[3]},
-                {word: global.posWords[4]},
-                {word: global.posWords[5]},
-                {word: global.posWords[6]},
-                {word: global.posWords[7]}
+         attribute1 : {
+			name : 'Male', //Will appear in the data.
+			title : {
+				media : {word : 'Male'}, //Name of the category presented in the task.
+				css : {color:'#0000FF','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Man'},
+				{word: 'Son'},
+				{word: 'Father'},
+				{word: 'Boy'},
+				{word: 'Uncle'},
+				{word: 'Grandpa'},
+				{word: 'Husband'},
+				{word: 'Male'}	
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},	
+		attribute2 :	{
+			name : 'Female', //Will appear in the data.
+			title : {
+				media : {word : 'Female'}, //Name of the category presented in the task.
+				css : {color:'#0000FF','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Mother'},
+				{word: 'Wife'},
+				{word: 'Aunt'},
+				{word: 'Woman'},
+				{word: 'Girl'},
+				{word: 'Female'},
+				{word: 'Grandma'},
+				{word: 'Daughter'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		category1 : 
+		{
+			name : 'Science', 
+			title : {
+				media : {word : 'Science'}, 
+				css : {color:'#31940F','font-size':'1.8em'}, 
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Astronomy'},
+				{word: 'Math'},
+				{word: 'Chemistry'},
+				{word: 'Physics'},
+				{word: 'Biology'},
+				{word: 'Geology'},
+				{word: 'Engineering'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'2.3em'} 
+		},
+		category2 : 
+		{
+			name : 'Liberal Arts', 
+			title : {
+				media : {word : 'Liberal Arts'}, 
+				css : {color:'#31940F','font-size':'1.8em'}, 
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'History'},
+				{word: 'Arts'},
+				{word: 'Humanities'},
+				{word: 'English'},
+				{word: 'Philosophy'},
+				{word: 'Music'}, 
+				{word: 'Literature'}
             ],
             //Stimulus css
             stimulusCss : {color:'#0000FF','font-size':'2.3em'}
