e13e91d646f0c7091283ecce527a3667bd2c911c
KatBrad <160325347+KatBrad@users.noreply.github.com>
2024-05-08 17:11:40 +0300

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -5,7 +5,7 @@ define(['managerAPI',
 	//You can use the commented-out code to get parameters from the URL.
 	const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const pt = urlParams.get('pt');
+    const textInput = urlParams.get('textInput');
 
 	var API    = new Manager();
 	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
